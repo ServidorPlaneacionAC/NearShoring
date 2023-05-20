ed1b9f5bd82a505d70f867baffd1e19c8cf91b96
juandapino <132597825+juandapino@users.noreply.github.com>
2023-05-20 09:56:15 -0500

    Update op.py

--- op.py
+++ op.py
@@ -20,9 +20,11 @@ def m(valores,valores_2):
     lt_puertopuerto_1=valores[10]
     lt_gz_1=valores[11]
     lt_gzplanta_1=valores[12]
-    transporte_gz_1=valores[13]
+    tarifa_alm_1=valores[13]
+    
 
     #campos_1 calculados
+    
     lt_completo_1=lt_tiempoadmon_1+lt_puertopuerto_1+lt_plantapuerto_1+lt_gz_1+lt_gzplanta_1
     lt_logistico_1=lt_puertopuerto_1+lt_plantapuerto_1+lt_gz_1+lt_gzplanta_1
     zona_amarilla_1=lt_plantapuerto_1*adu_1
@@ -30,12 +32,16 @@ def m(valores,valores_2):
     zona_rojaalta_1=zona_rojabase_1*factor_var_1
     zona_verde_1=max(moq_1,frecuencia_1*adu_1,lt_plantapuerto_1*adu_1*factor_lt_1)
     inv_prom_1=zona_rojabase_1+zona_rojaalta_1+(zona_verde_1/2)
-    taf_gz_1=1.07*precio_compra_1
+    indice_flujo_1=zona_verde_1/adu_1
+    diferencial_1=lt_logistico_1-semanas_cxp_1
+    taf_gz_1=0*precio_compra_1
     costo_inv_1=precio_compra_1*inv_prom_1
-    costo_cap_1=(((1+0.12)**(1/52))-1)*lt_plantapuerto_1*costo_inv_1
-    costo_maninv_1=(inv_prom_1/estandar_pos_1)*taf_gz_1*(lt_plantapuerto_1/4.3)
+    costo_nacionalizacion_1=taf_gz_1*cantidad_1
+    costo_transportegz-planta_1=0*cantidad_1
+    costo_cap_1=(diferencial_1+indice_flujo_1)*adu_1*(((1+0.12)**(1/52))-1)*precio_compra_1
+    costo_maninv_1=inv_prom_1*(tarifa_alm_1*4.3)*indice_flujo_1
     costo_compra_1=precio_compra_1*cantidad_1
-    costo_total_1=costo_maninv_1+costo_compra_1+costo_cap_1
+    costo_total_1=costo_maninv_1+costo_compra_1+costo_cap_1+costo_nacionalizacion_1+costo_transportegz-planta_1
     costo_unitario_1=costo_total_1/cantidad_1
 
     
@@ -52,9 +58,10 @@ def m(valores,valores_2):
     lt_puertopuerto=valores_2[10]
     lt_gz=valores_2[11]
     lt_gzplanta=valores_2[12]
-    transporte_gz=valores_2[13]
+    tarifa_alm=valores[13]
     precio_compra=valores_2[14]
     #campos calculados
+    
     lt_completo=lt_tiempoadmon+lt_puertopuerto+lt_plantapuerto+lt_gz+lt_gzplanta
     lt_logistico=lt_puertopuerto+lt_plantapuerto+lt_gz+lt_gzplanta
     zona_amarilla=lt_plantapuerto*adu
@@ -62,12 +69,16 @@ def m(valores,valores_2):
     zona_rojaalta=zona_rojabase*factor_var
     zona_verde=max(moq,frecuencia*adu,lt_plantapuerto*adu*factor_lt)
     inv_prom=zona_rojabase+zona_rojaalta+(zona_verde/2)
-    taf_gz=1.07*precio_compra
+    indice_flujo=zona_verde/adu
+    diferencial=lt_logistico-semanas_cxp
+    taf_gz=0.07*precio_compra
     costo_inv=precio_compra*inv_prom
-    costo_cap=(((1+0.12)**(1/52))-1)*lt_plantapuerto*costo_inv
-    costo_maninv=(inv_prom/estandar_pos)*taf_gz*(lt_plantapuerto/4.3)
+    costo_nacionalizacion=taf_gz*cantidad
+    costo_transportegz-planta=200*cantidad
+    costo_cap=(diferencial+indice_flujo)*adu*(((1+0.12)**(1/52))-1)*precio_compra
+    costo_maninv=inv_prom*(tarifa_alm*4.3)*indice_flujo
     costo_compra=precio_compra*cantidad
-    costo_total=costo_maninv+costo_compra+costo_cap
+    costo_total=costo_maninv+costo_compra+costo_cap+costo_nacionalizacion+costo_transportegz-planta
     costo_unitario_0=costo_total/cantidad
 
 
@@ -112,6 +123,7 @@ nombres=(
 "lt_gz",
 "lt_gzplanta",
 "transporte_gz",
+"tarifa almacenamiento",
 "precio_compra")
 
 # Columna izquierda
