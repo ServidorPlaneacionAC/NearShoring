c2f26dcccf7d585a65ce963441dce0173a25aec2
juandapino <132597825+juandapino@users.noreply.github.com>
2023-05-29 12:11:45 -0500

    Update op.py

--- op.py
+++ op.py
@@ -119,7 +119,7 @@ def m(valores,valores_2):
     prob += uodi == 0
     status = prob.solve()
 #     return (p_1.value())
-    return f"ingreso bueno {p_1.value() ,value(uodi),value(diferencial_ct),value(capital_invertido_1),value(capital_invertido),value(diferencial_1),value(inv_prom_sem_1)}"
+    return f"ingreso bueno {p_1.value() ,value(uodi),value((diferencial_1+inv_prom_sem_1)),value(adu_1),value(costo_nacionalizacion_1)}"
     
 
 st.title("Nearshoring")
