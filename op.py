2c7b17f0df319c64a1991872e4475f3475a25927
juandapino <132597825+juandapino@users.noreply.github.com>
2023-06-01 16:18:07 -0500

    Update op.py

--- op.py
+++ op.py
@@ -1,67 +1,6 @@
 import streamlit as st
 from pulp import *
-from streamlit import session_state
 
-def main():
-    st.sidebar.title("Escenarios")
-    options = ["Escenario nacional", "Escenario internacional", "Resultados"]
-    
-    agregar_costo_capital = False
-    agregar_costo_capital=st.sidebar.checkbox("Costo capital", value=agregar_costo_capital)  
-    if st.button("Ejecutar método"):
-        if agregar_costo_capital:
-            resultado = eva(valores,valores_2)
-        else:
-            resultado = uodi(valores,valores_2)
-    if "choice" not in session_state:
-        session_state.choice = "Escenario nacional"
-    choice = st.sidebar.selectbox("Selecciona una sección", options, options.index(session_state.choice))
-    session_state.choice = choice
-
-
-        
-    nombres=(
-    "Cantidad",
-    "Frecuencia",
-    "Lead time planta-puerto",
-    "Semanas cxp",
-    "Moq",
-    "Adu",
-    "Factor lead time",
-    "Factor variación",
-    "Estandar posición",
-    "lead time tiempo-admon",
-    "lead time puerto-puerto",
-    "lead time gestión cargo",
-    "lead time gz-planta",
-    "Tarifa almacenamiento",
-    "Precio compra")
-    nombres_2=(
-    "Cantidad",
-    "Frecuencia",
-    "Icoterm",   
-    "lead time planta-puerto",
-    "Semanas cxp",
-    "Moq",
-    "Adu",
-    "Factor lead time",
-    "Factor variación",
-    "Estandar posición",
-    "lead time tiempo-admon",
-    "lead time puerto-puerto",
-    "lead time gestión cargo",
-    "lead time gz-planta",
-    "Tarifa almacenamiento",
-    "Precio compra")
-    
-    if choice == "Escenario nacional":
-        Escenario_nacional(nombres)
-    elif choice == "Escenario internacional":
-        Escenario_internacional(nombres)
-    elif choice == "Resultados":
-        Resultados()   
-    
-    st.title("Nearshoring")
 
 def uodi(valores,valores_2):
     # Aquí va tu método m
@@ -84,7 +23,6 @@ def uodi(valores,valores_2):
     lt_gzplanta_1=valores[12]
     tarifa_alm_1=valores[13]
     
-
     #campos_1 calculados
     
     lt_completo_1=lt_tiempoadmon_1+lt_puertopuerto_1+lt_plantapuerto_1+lt_gz_1+lt_gzplanta_1
@@ -154,10 +92,10 @@ def uodi(valores,valores_2):
     costo_ebitda=costo_maninv+costo_compra+costo_nacionalizacion+costo_transportegz_planta
     costo_unitario_0=costo_total/cantidad
     capital_invertido=((diferencial+inv_prom_sem)*(adu)*(precio_compra))+(costo_nacionalizacion)
-    
-    
+
+
     #calculo variables financieras
-    
+
     ebitda=costo_ebitda_1-costo_ebitda
     impuestos=ebitda*0.26
     uodi=ebitda-impuestos
@@ -165,20 +103,16 @@ def uodi(valores,valores_2):
     costo_capital=(diferencial_ct)*(((1+0.12)**(1/52))-1)
     eva=uodi-costo_capital
     #roic=uodi/diferencial_ct
-
     c_0 = costo_unitario_0  # Costo del producto 0
     c_1 = costo_unitario_1  # Costo del producto 1
     p_0= precio_compra
     # Define las variables de cambio de precio
     #p_0 = LpVariable("p_0", lowBound=0)
-
-
     # Agrega restricciones
     #prob += p_0 <= 1
     #prob += p_1 <= 1
     #prob += p_0 >= 0
     prob += uodi >= 0
-
     # Define la función objetivo
     #prob += c_1 - c_0 == p_1 - p_0 # Minimiza la diferencia de costos totales
     prob += uodi == 0
@@ -206,10 +140,10 @@ def eva(valores,valores_2):
     lt_gz_1=valores[11]
     lt_gzplanta_1=valores[12]
     tarifa_alm_1=valores[13]
-    
+
 
     #campos_1 calculados
-    
+
     lt_completo_1=lt_tiempoadmon_1+lt_puertopuerto_1+lt_plantapuerto_1+lt_gz_1+lt_gzplanta_1
     lt_logistico_1=lt_puertopuerto_1+lt_plantapuerto_1+lt_gz_1+lt_gzplanta_1
     zona_amarilla_1=lt_plantapuerto_1*adu_1
@@ -219,7 +153,7 @@ def eva(valores,valores_2):
     inv_prom_1=zona_rojabase_1+zona_rojaalta_1+(zona_verde_1/2)
     inv_prom_sem_1=inv_prom_1/adu_1
     diferencial_1=lt_logistico_1-semanas_cxp_1
-    
+
     taf_gz_1=0
     costo_inv_1=precio_compra_1*inv_prom_1
     costo_nacionalizacion_1=taf_gz_1*cantidad_1
@@ -231,7 +165,7 @@ def eva(valores,valores_2):
     costo_ebitda_1=costo_maninv_1+costo_compra_1
     costo_unitario_1=costo_total_1/cantidad_1
     capital_invertido_1=((diferencial_1+inv_prom_sem_1)*(adu_1))*(precio_compra_1)
-    
+
     cantidad=valores_2[0]
     frecuencia=valores_2[1]
     icoterm = valores_2[2]
@@ -249,7 +183,7 @@ def eva(valores,valores_2):
     tarifa_alm=valores_2[14]
     precio_compra=valores_2[15]
     #campos calculados
-    
+
     lt_completo=lt_tiempoadmon+lt_puertopuerto+lt_plantapuerto+lt_gz+lt_gzplanta
     lt_logistico= icoterm  
     if icoterm == "EXWORK":
@@ -266,7 +200,7 @@ def eva(valores,valores_2):
     inv_prom_sem=inv_prom/adu
     diferencial=lt_logistico-semanas_cxp
     taf_gz=0.07*precio_compra
-    
+
     costo_inv=precio_compra*inv_prom
     costo_nacionalizacion=taf_gz*cantidad
     costo_transportegz_planta=200*cantidad
@@ -277,10 +211,10 @@ def eva(valores,valores_2):
     costo_ebitda=costo_maninv+costo_compra+costo_nacionalizacion+costo_transportegz_planta
     costo_unitario_0=costo_total/cantidad
     capital_invertido=((diferencial+inv_prom_sem)*(adu)*(precio_compra))+(costo_nacionalizacion)
-    
-    
+
+
     #calculo variables financieras
-    
+
     ebitda=costo_ebitda_1-costo_ebitda
     impuestos=ebitda*0.26
     uodi=ebitda-impuestos
@@ -308,36 +242,62 @@ def eva(valores,valores_2):
     status = prob.solve()
 #     return (p_1.value())
     return [p_1.value(),value(uodi),value(ebitda),value(eva),value(diferencial_ct)]
-
-
-
-
- 
-
+# nombres
+nombres=(
+"Cantidad",
+"Frecuencia",
+"Lead time planta-puerto",
+"Semanas cxp",
+"Moq",
+"Adu",
+"Factor lead time",
+"Factor variación",
+"Estandar posición",
+"lead time tiempo-admon",
+"lead time puerto-puerto",
+"lead time gestión cargo",
+"lead time gz-planta",
+"Tarifa almacenamiento",
+"Precio compra")
+nombres_2=(
+"Cantidad",
+"Frecuencia",
+"Icoterm",   
+"lead time planta-puerto",
+"Semanas cxp",
+"Moq",
+"Adu",
+"Factor lead time",
+"Factor variación",
+"Estandar posición",
+"lead time tiempo-admon",
+"lead time puerto-puerto",
+"lead time gestión cargo",
+"lead time gz-planta",
+"Tarifa almacenamiento",
+"Precio compra")
+st.sidebar.title("Escenarios")
+st.sidebar.button("Escenario nacional")
+st.sidebar.button("Escenario internacional")
+agregar_costo_capital = False
+agregar_costo_capital=st.sidebar.checkbox("Costo capital", value=agregar_costo_capital)  
+st.title("Nearshoring")
 # Definir la disposición en dos columnas
-
-
-def Escenario_nacional(nombres):
+col1, col2 = st.beta_columns(2)
+with col1:
     st.subheader("Escenario nacional")
+    valores = []
+    col1_1, col1_2 = st.beta_columns(2)
     
-    if "valores" not in session_state:
-        session_state.valores = []
-        col1_1, col1_2 = st.beta_columns(2)
+    with col1_1:
+        for i in range(8):
+            valores.append(st.number_input(f"{nombres[i]}", step=0.1, min_value=0.0, max_value=100000.0))
     
-        with col1_1:
-            for i in range(8):
-                valores.append(st.number_input(f"{nombres[i]}", step=0.1, min_value=0.0, max_value=100000.0))
-        with col1_2:
-            for i in range(8, 14):
-                valores.append(st.number_input(f"{nombres[i]}", step=0.1, min_value=0.0, max_value=100000.0))
-    else:
-        valores =session_state.valores
-
+    with col1_2:
+        for i in range(8, 14):
+            valores.append(st.number_input(f"{nombres[i]}", step=0.1, min_value=0.0, max_value=100000.0))
     
-    if st.button("Enviar"):
-        session_state.valores = valores
-
-def Escenario_internacional(nombres):
+with col2:
     st.subheader("Escenario internacional")
     col2_1, col2_2 = st.beta_columns(2)
     valores_2 = []
@@ -352,21 +312,18 @@ def Escenario_internacional(nombres):
     with col2_2:
         for i in range(8, 16):
             valores_2.append(st.number_input(f"{nombres_2[i]}", step=0.01, min_value=0.00, max_value=100000.00))
-    
 
 
 # Crear botón para ejecutar el métodorun
-def Resultados():
+if st.button("Ejecutar método"):
+    if agregar_costo_capital:
+        resultado = eva(valores,valores_2)
+    else:
+        resultado = uodi(valores,valores_2)
     st.write(f"El resultado es: {resultado[0]}")
     st.write(f"UODI: {resultado[1]}")
     st.write(f"EBITDA: {resultado[2]}")
-    st.write(f"EVA: {resultado[3]}")
-    st.write(f"ROIC: {0 if resultado[4] == 0 else resultado[1]/resultado[4]}")
-    
-    
-if __name__ == "__main__":
-    main()
-    
+
 
 
 
