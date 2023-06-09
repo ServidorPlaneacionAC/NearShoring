33438acf4a98be693679b24c0e8df6f0d43c3320
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-09 18:19:27 -0500

    Update op.py

--- op.py
+++ op.py
@@ -114,7 +114,7 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
             session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm)       
             valores[-1]=valores[-1]*session_state.trm     
             session_state.valor_en_pesos=valores[-1]
-         else:
+        else:
             session_state.valor_en_pesos=formulario1[nombres[-1]]
             
     if st.button("Guardar"): 
