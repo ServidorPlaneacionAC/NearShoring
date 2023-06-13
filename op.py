e34666bc2378fa41774085d1f2a246b0b6cd8da6
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-12 22:33:20 -0500

    Update op.py

--- op.py
+++ op.py
@@ -4,10 +4,14 @@ from pulp import *
 
 def main():
     if "error" not in session_state:
+        #Variable generada para validaciones, inicia en True porque al iniciar todos los datos estan sin diligenciar
+        #si se diligencian y quedan con valores lógicos se apaga y permite guardar la informacion en las variables estados de sesion
         session_state.error=True
     if "trm" not in session_state:
+        #Variable para almacenar la trm en los cambios de formularios
         session_state.trm=4800.00
     if "valor_en_pesos" not in session_state:
+        #Variable para almacenar el valor_en_pesos en los cambios de formularios
         session_state.valor_en_pesos=00.00
       
     nombres=(
@@ -49,6 +53,7 @@ def main():
     
     agregar_costo_capital = False
     agregar_costo_capital=st.sidebar.checkbox("Costo capital", value=agregar_costo_capital)  
+    #Si se tiene en cuenta el costo capital se optimiza sobre el eva, sino obre el uodi
     
     if choice == "Escenario Nacional":
         if "formulario1" not in session_state:
@@ -63,6 +68,8 @@ def main():
     elif choice == "Resultados": 
         
         if "formulario2" not in session_state or "formulario1" not in session_state or session_state.error:
+            #las variables de estado formulario 1 y 2 solo se inicializan si se toman como esta alternaativa en el menu de navegacion
+            #Falta corregir que pasa si diligencio un formulario bien, entro al otro y no guardo
             st.error("no se ha diligenciado algun escenario")
         else:
             valores=[]
@@ -71,7 +78,7 @@ def main():
                 valores.append(session_state.formulario1[nombre])
             for nombre in nombres_2:
                 valores_2.append(session_state.formulario2[nombre])
-
+            #Almaceno valores en listas para pasarolo como parametros a las funciones eva y uodi 
             if agregar_costo_capital:
                 resultado = eva(valores,valores_2)
             else:
@@ -88,12 +95,20 @@ def resultados(resultado):
     st.write(f"ROIC: {0 if resultado[4] == 0 else resultado[1]/resultado[4]}")
 
 def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internacional=False):
+    '''Funcion que genera los formularios para evaluar las oportunidades de inversión, recibe nombre del escenario, lista nombres que
+        indica los campos del formulario, formulario1 que es un diccionario que donde se almacena los valores de los campos, si no se pasa por 
+        parametro inicializa las variables en 0 y transaccion_internacional que es un booleano que indica si se debe o no añadir la opción de 
+        indicar el precio en pesos y trasnformarlo'''
+    
+    #inicializo variables locales para uso posterior
     costo_dolares=0.0
     checkbox_operacion_dolarizado=False
     st.title(titulo)
     if formulario1 is None:
+        #inicializo los valores en 0.0 o vacios si formulario1 no esta declarado
         formulario1 = {nombre: (0.0 if nombre!= "Icoterm" else "") for nombre in nombres}
     
+    #indico que mostrare la informacion en 2 columnas e inicializo variable local valores que es donde voy a guardadr temporalmente las respuestas
     col1_1, col1_2 = st.columns(2)
     valores = []
     
@@ -103,42 +118,39 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
                 valores.append(st.text_input(nombres[i], value=formulario1[nombres[i]]))
             else:    
                 valores.append(st.number_input(nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
-    
+        
         if transaccion_internacional==True:
+            #genero proceso extra para transformar de dolares a pesos y pesos a dolares
             checkbox_operacion_dolarizado = st.checkbox("indicar el precio en dolares")
             if checkbox_operacion_dolarizado:
+                '''si se selecciono el checkbox_operacion_dolarizado entonces ingresa y hace la variable 
+                    costo_dolares=session_state.valor_en_pesos(inicializada en el main con valor 0.0)/session_state.trm(inicializada en el main con valor 4.800) 
+                    luego genero un campo para indicar trm al momento de la negociación, igual que el campo para indicar el precio en dolares,
+                    luego hago guardo en la variable de estado valor_en_pesos el valor en pesos de la transaccion indicada
+                    '''
                 costo_dolares=session_state.valor_en_pesos/session_state.trm
-                session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm) 
-                costo_dolares=st.number_input("Precio compra en dolares", step=0.1, min_value=0.0, max_value=100000.0, value=costo_dolares)       
-    #             valores[-1]=valores[-1]*session_state.trm     
+                session_state.trm=st.number_input("Valor TRM", step=0.0001, min_value=0.0, max_value=100000.0, value=session_state.trm) 
+                costo_dolares=st.number_input("Precio compra en dolares", step=0.1, min_value=0.0, max_value=100000.0, value=costo_dolares) 
                 session_state.valor_en_pesos=costo_dolares*session_state.trm  
     
     with col1_2:
+        '''Si la transaccion no es dolarizada se traen todos los campos del formulario y ya'''
         if transaccion_internacional==True:
             for i in range(int(len(nombres)/2), len(nombres)-1):
                 valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
             if not checkbox_operacion_dolarizado:
+                '''Si no se ha seleccionado la alternativa de operacion dolarizda indico que el valor de la ultima variable puesta en nombre es lo que esta
+                    en la variable de estado valor_en_pesos(inicializada en el main con valor 0.0 y modificada cuando selecciono la operacion dolarizada
+                    y llevo a valor_en_pesos el valor que se escriba en este campo)'''
                 valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos))
                 session_state.valor_en_pesos=valores[-1]
             else:            
+                #traigo valor_en_pesos pero no permito su edicion
                 valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos,disabled=True))
         else:
             for i in range(int(len(nombres)/2), len(nombres)):
                 valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
-            
-            
-#     if transaccion_internacional==True:
-#         checkbox_operacion_dolarizado = st.checkbox("indicar el precio en dolares")
-#         if checkbox_operacion_dolarizado:
-#             session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm)       
-#             valores[-1]=valores[-1]*session_state.trm     
-#             session_state.valor_en_pesos=valores[-1]
-#             st.write(valores[-1])
-#             st.number_input(nombres[-1],  step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos, key="disabled_number_input", disabled=True)
-
-#         else:
-#             session_state.valor_en_pesos=formulario1[nombres[-1]]
-            
+                 
     if st.button("Guardar"): 
         if 0.0 in valores or "" in valores:
             session_state.error=True
@@ -153,8 +165,6 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
     return formulario1
 
 def eva(valores,valores_2):
-    # Aquí va tu método m
-    # Crear problema de minimización
     prob = LpProblem("Mi problema de optimización", LpMinimize)
     p_1 = LpVariable("p_1", lowBound=0)
     precio_compra_1=p_1
