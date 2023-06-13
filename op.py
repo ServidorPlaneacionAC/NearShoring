2fa1d3162317fed0231459ce5f1b2adcdde4806f
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-12 20:53:37 -0500

    Update op.py

--- op.py
+++ op.py
@@ -102,22 +102,33 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
             else:    
                 valores.append(st.number_input(nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
     
-    with col1_2:
-        for i in range(int(len(nombres)/2), len(nombres)-1):
-            valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
-        valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos))
-    
     if transaccion_internacional==True:
         checkbox_operacion_dolarizado = st.checkbox("indicar el precio en dolares")
         if checkbox_operacion_dolarizado:
             session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm)       
             valores[-1]=valores[-1]*session_state.trm     
-            session_state.valor_en_pesos=valores[-1]
-            st.write(valores[-1])
-            st.number_input(nombres[-1],  step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos, key="disabled_number_input", disabled=True)
-
+            session_state.valor_en_pesos=valores[-1]    
+    
+    with col1_2:
+        for i in range(int(len(nombres)/2), len(nombres)-1):
+            valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
+        if checkbox_operacion_dolarizado:
+            valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos))
         else:
-            session_state.valor_en_pesos=formulario1[nombres[-1]]
+            valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos,disabled=True))
+       
+            
+#     if transaccion_internacional==True:
+#         checkbox_operacion_dolarizado = st.checkbox("indicar el precio en dolares")
+#         if checkbox_operacion_dolarizado:
+#             session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm)       
+#             valores[-1]=valores[-1]*session_state.trm     
+#             session_state.valor_en_pesos=valores[-1]
+#             st.write(valores[-1])
+#             st.number_input(nombres[-1],  step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos, key="disabled_number_input", disabled=True)
+
+#         else:
+#             session_state.valor_en_pesos=formulario1[nombres[-1]]
             
     if st.button("Guardar"): 
         if 0.0 in valores or "" in valores:
