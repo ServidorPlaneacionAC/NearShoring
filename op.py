1eba421b914b618af6c93850ac25471381722e8c
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-12 21:28:14 -0500

    Update op.py

--- op.py
+++ op.py
@@ -107,6 +107,7 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
         if transaccion_internacional==True:
             checkbox_operacion_dolarizado = st.checkbox("indicar el precio en dolares")
             if checkbox_operacion_dolarizado:
+                costo_dolares=session_state.valor_en_pesos/session_state.trm
                 session_state.trm=st.number_input("Valor TRM", step=0.1, min_value=0.0, max_value=100000.0, value=session_state.trm) 
                 costo_dolares=st.number_input("Precio compra en dolares", step=0.1, min_value=0.0, max_value=100000.0, value=costo_dolares)       
     #             valores[-1]=valores[-1]*session_state.trm     
@@ -117,7 +118,7 @@ def mostrar_formulario_1(titulo,nombres, formulario1=None, transaccion_internaci
             valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
         if not checkbox_operacion_dolarizado:
             valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos))
-        else:
+        else:            
             valores.append(st.number_input(nombres[-1],key=nombres[-1], step=0.1, min_value=0.0, max_value=100000.0, value=session_state.valor_en_pesos,disabled=True))
        
             
