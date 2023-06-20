6d35b218fd7f30b54733aecfa2db93807a331205
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-20 12:21:01 -0500

    invertir indicadores fieros

--- streamlit_frm.py
+++ streamlit_frm.py
@@ -19,9 +19,9 @@ class streamlit_frm:
                                  [[resultado[0][0],
                                    "{:.2f}%".format((resultado[0][0]-self.valor_en_pesos)/self.valor_en_pesos*100),
                                     round(resultado[0][1],2),
-                                    round(resultado[0][2],2),
-                                    round(resultado[0][3],2),
-                                    round(0 if resultado[0][4] == 0 else resultado[0][1]/resultado[0][4],2)]], 
+                                    -round(resultado[0][2],2),
+                                    -round(resultado[0][3],2),
+                                    -round(0 if resultado[0][4] == 0 else resultado[0][1]/resultado[0][4],2)]], 
                                  columns=['Precio a pagar','variación respecto al original','UODI','EBITDA','EVA','ROIC'])
     st.write(tabla_resultado)
     
@@ -30,18 +30,18 @@ class streamlit_frm:
                                  [[resultado[i][0],
                                    "{:.2f}%".format((resultado[i][0]-self.valor_en_pesos)/self.valor_en_pesos*100),
                                     round(resultado[i][1],2),
-                                    round(resultado[i][2],2),
-                                    round(resultado[i][3],2),
-                                    round(0 if resultado[i][4] == 0 else resultado[i][1]/resultado[i][4],2)] for i in range(len(resultado))], 
+                                    -round(resultado[i][2],2),
+                                    -round(resultado[i][3],2),
+                                    -round(0 if resultado[i][4] == 0 else resultado[i][1]/resultado[i][4],2)] for i in range(len(resultado))], 
                                  columns=['Precio a pagar','variación respecto al original','UODI','EBITDA','EVA','ROIC'])
     st.write(tabla_resultado[1:]) 
     
     
     precios = [resultado[i][0] for i in range(len(resultado))]
-    UODI = [resultado[i][1] for i in range(len(resultado))]
+    UODI = [-resultado[i][1] for i in range(len(resultado))]
     Linea_Base = [0 for i in range(len(resultado))]
-    EVA = [resultado[i][3] for i in range(len(resultado))]
-    EBITDA = [resultado[i][2] for i in range(len(resultado))]
+    EVA = [-resultado[i][3] for i in range(len(resultado))]
+    EBITDA = [-resultado[i][2] for i in range(len(resultado))]
     self.grafica_lineas([precios,Linea_Base,EVA,EBITDA],[UODI],["Precios por unidad"],["UODI"])
     
   def mostrar_formulario_1(self,titulo,nombres, formulario1=None, transaccion_internacional=False):
