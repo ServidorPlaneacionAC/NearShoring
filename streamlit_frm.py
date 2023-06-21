c64ba737d18cf48f4d04a4c6559f1251b7c1d392
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-21 17:27:01 -0500

    valida datos

--- streamlit_frm.py
+++ streamlit_frm.py
@@ -153,10 +153,10 @@ class streamlit_frm:
                 valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))
             if  opcion_iconterm == "EXWORK": 
                 for i in range(len(nombres)-5, len(nombres)-1):                              
-                    valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]],disabled=True))
+                    valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=0.0,disabled=True))
             else:
                 for i in range(len(nombres)-5, len(nombres)-1):                              
-                    valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=0.0))    
+                    valores.append(st.number_input(nombres[i],key=nombres[i], step=0.1, min_value=0.0, max_value=100000.0, value=formulario1[nombres[i]]))    
             if not checkbox_operacion_dolarizado:
 #                 Si no se ha seleccionado la alternativa de operacion dolarizda indico que el valor de la ultima variable puesta en nombre es lo que esta
 #                     en la variable de estado valor_en_pesos(inicializada en el main con valor 0.0 y modificada cuando selecciono la operacion dolarizada
