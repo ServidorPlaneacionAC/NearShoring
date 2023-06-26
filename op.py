edeb985d146626e98d087e836554b8c57767c57d
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2023-06-26 16:49:12 -0500

    mmm

--- op.py
+++ op.py
@@ -94,9 +94,9 @@ def main():
                 valores_2.append(session_state.formulario2[nombre])
             valores_2.append(0.0)
             for nombre in nombres_2[-6:-2]: #se suman todos los valores al ultimo
-                st.write(f"{nombre} {session_state.formulario2[nombre]} ")
                 valores_2[-1]+=(session_state.formulario2[nombre])
             valores_2[-1]=valores_2[-1]*session_state.formulario2["Factor de importacion"]
+            st.write(f"{valores_2[-1]} ")
             #Almaceno valores en listas para pasarolo como parametros a las funciones eva y uodi 
             #Genero ciclos para crear tabla de valor y mostrar valores cercanos, los guardo en una matriz de 2 x 2 
             # la envío al metodo resultados
