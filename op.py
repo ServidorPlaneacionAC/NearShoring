88693361c825ac49a96eefae670126d0acf53c6f
juandapino <132597825+juandapino@users.noreply.github.com>
2023-07-10 11:10:50 -0500

    jd

--- op.py
+++ op.py
@@ -68,8 +68,8 @@ def main():
         session_state.esc_retador = "Escenario Nacional"
     frm= streamlit_frm(session_state.valor_en_pesos,session_state.trm)
     if choice == "Nuevo Escenario":
-        esc_retador = st.selectbox("Selecciona si el escenario es nacional o internacional", opciones,opciones.index(esc_retador))
-        if "Escenario Nacional" == esc_retador:
+        session_state.esc_retador = st.selectbox("Selecciona si el escenario es nacional o internacional", opciones,opciones.index(session_state.esc_retador))
+        if "Escenario Nacional" == session_state.esc_retador:
 
             if "formulario1" not in session_state:
                 session_state.formulario1,session_state.valor_en_pesos,session_state.error,session_state.trm = frm.mostrar_formulario_1(choice,nombres)
