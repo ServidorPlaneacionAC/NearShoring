55ce48808cff1dbf337bc01deb54caeeffaa1c5d
Santiago Marulanda V <santiagomv.96@gmail.com>
2023-08-03 16:50:04 -0500

    dd

--- utils.py
+++ utils.py
@@ -250,7 +250,7 @@ def uodi(valores,valores_2,tasa):
     st.write(f"{value(zona_rojaalta)}")
     st.write(f"{value(zona_verde)}")
     
-    st.write(f"{value(zona_amarilla_1)}")
+    st.write(f"{value(zona_amarilla_1)} amarilla nacional")
     st.write(f"{value(zona_rojabase_1)}")
     st.write(f"{value(zona_rojaalta_1)}")
     st.write(f"{value(zona_verde_1)}")
