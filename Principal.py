bfa68482007c9d6f26657c477dae2ff710c9e496
juandapino <132597825+juandapino@users.noreply.github.com>
2023-10-12 16:48:24 -0500

    jd

--- Principal.py
+++ Principal.py
@@ -27,7 +27,7 @@ def main ():
              'Esenario': "todos",
              'Tipo_Dato': "int",
              'Valor': -1},
-         4: {'Nombre': "Cantidad mínima a pedir: Cantidad dada en unidad de medida determinada por el negociador",
+         4: {'Nombre': "Inventario promedio: Inventario promedio del material",
              'Tipo': "Obligatorio",
              'Esenario': "todos",
              'Tipo_Dato': "int",
@@ -37,27 +37,22 @@ def main ():
              'Esenario': "todos",
              'Tipo_Dato': "int",
              'Valor': -1},
-         6: {'Nombre': "Factor lead time: De 0 a 1 qué tan volátil es el lead time, siendo 0 muy seguro y 1 completamente variable",
+         6: {'Nombre': "Tarifa gestion cargo: Cobro del material en gestion cargo",
              'Tipo': "Obligatorio",
              'Esenario': "todos",
              'Tipo_Dato': "int",
              'Valor': -1},
-         7: {'Nombre': "Factor variación: De 0 a 1 qué tan volátil es el consumo, siendo 0 muy seguro y 1 completamente variable",
+         7: {'Nombre': "Costo transporte: Costo por unidad en el camion",
              'Tipo': "Obligatorio",
              'Esenario': "todos",
              'Tipo_Dato': "int",
              'Valor': -1},
-         8: {'Nombre': "Unidades por estiba: Cantidad dada en unidad de medida determinada por el negociador",
+         8: {'Nombre': "Tarifa almacenamiento por cada unidad de medida determinada por el negociador",
              'Tipo': "Obligatorio",
              'Esenario': "todos",
              'Tipo_Dato': "int",
              'Valor': -1},
-         9: {'Nombre': "Tarifa almacenamiento por cada unidad de medida determinada por el negociador",
-             'Tipo': "Obligatorio",
-             'Esenario': "todos",
-             'Tipo_Dato': "int",
-             'Valor': -1},
-         10: {'Nombre': "Precio a pagar",
+         9: {'Nombre': "Precio a pagar",
              'Tipo': "Obligatorio",
              'Esenario': "Actual",
              'Tipo_Dato': "int",
@@ -75,9 +70,27 @@ def main ():
         session_state.Dicc_Variables = mostrar_valores(Dicc_Variables,estados_checkboxes)
     with col1_2:
         session_state.Dicc_Variables2 = mostrar_valores(Dicc_Variables,estados_checkboxes, '2','Retador')
-
-def calcular_campos():    
-    pass
+    organizar_campos(session_state.Dicc_Variables)
+def organizar_campos(Diccionario): 
+    '''
+    Asigna los valores a los variables desde el diccionario
+    '''
+    cantidad = Diccionario[0]['Valor'] 
+    frecuencia = Diccionario[1]['Valor']
+    lead_time=Diccionario[2]['Valor']
+    condicion_pago= Diccionario[3]['Valor']
+    inv_prom = Diccionario[4]['Valor']
+    asu = Diccionario[5]['Valor']
+    tarifa_gz = Diccionario[6]['Valor']
+    costo_transporte=Diccionario[7]['Valor']
+    tarifa_alm = Diccionario[8]['Valor']
+ 
+def optimizacion(cantidad,frecuencia,lead_time,condicion_pago,inv_prom,asu,tarifa_gz,costo_transporte,tarifa_alm,precio,
+                 cantidad_1,frecuencia_1,lead_time_1,condicion_pago_1,inv_prom_1,asu_1,tarifa_gz_1,costo_transporte_1,tarifa_alm_1)
+    '''
+    Me calcula los costos y me realiza la optimización
+    '''
+    
 
 def mostrar_valores(diccionario,estados_checkboxes, ind='', escenario='Actual'):
     st.write(escenario)
