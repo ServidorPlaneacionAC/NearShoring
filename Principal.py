31aed6b539d926e2624efab1814f13dd241454b6
Santiago Marulanda V <santiagomv.96@gmail.com>
2023-11-14 16:29:14 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -120,6 +120,10 @@ def main ():
             grafica_lineas([df['Precio'].tolist(),Linea_Base,df['EVA'].tolist(),df['EBITDA'].tolist()],df['UODI'].tolist(),["Precios por unidad"],["UODI"])
         else:
             resultado=(optimizacion(*valores_dicc_1, float(session_state.Dicc_Variables[10]["Valor"]), *valores_dicc_2,'EVA',int(session_state.Dicc_Variables2[11]['Valor'])))
+            if resultado[3]>0:
+                st.error(f'Si aceptamos la propuesta del nuevo proveedor estariamos perdiendo {resultado[3]} en EVA')
+            else:
+                st.success(f'Si **NO** aceptamos la propuesta del nuevo proveedor estariamos perdiendo {-resultado[3]} en EVA ')
             st.write(pd.DataFrame([resultado[:4]], columns=['Precio','UODI','EBITDA','EVA']))
             
 
